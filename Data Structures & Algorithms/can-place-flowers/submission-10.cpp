683e7b83a01d591a8d48508c3c346768e44c56fc
thanhbinh170708-wq <thanhbinh170708@gmail.com>
2026-08-02 19:30:55 +0700

    Add: can-place-flowers - submission-10

---
+++ Data Structures & Algorithms/can-place-flowers/submission-10.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool canPlaceFlowers(vector<int>& flowerbed, int n) {
+    int count = 0;
+    for (int i = 0; i < flowerbed.size(); i++)
+    {
+        if (flowerbed[i] == 0 && ((i - 1 < 0 || flowerbed[i-1] == 0) && (i+1 == flowerbed.size() || flowerbed[i+1] == 0 )) )
+        {
+            
+            flowerbed[i] = 1;
+            count++;
+        }
+
+    }  
+    
+    
+    if (n <= count)
+    {
+        return true;
+    } 
+    else
+    {
+        return false;
+    }
+    }
+};
